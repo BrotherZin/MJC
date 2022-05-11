42783b74f594a539b307efa382a9b479ff538317
BrotherZin <ahj13k@naver.com>
2022-05-11 11:01:01 +0900

    학교 과제 겸 백업용

---
+++ board/server/routes/board.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+router.post("/write", async function (req, res) {
+    await Board.create(req.body)
+    res.json({
+        result: "ok"
+    })
+})
+var Pager = require("node-jyh-pager")
+var pager = new Pager({
+    itemPerPage: 5,
+})
+router.post("/list", async function (req, res) {
+
+    var page = req.body.page
+    if (!page) {
+        page = 1
+    }
+    var itemPerPage = 5
+    var offset = pager.getSkip(page)
+    var offset = (page - 1) * itemPerPage
+    var boardList = await Board.findAll({
+        limit: itemPerPage,
+        offset: offset,
+        orders: [["writeTime", "DESC"]]
+    })
+    var count = await Board.count()
+    var nav = pager.getBottomNav(page, count)
+    var pageCount = nav.totalPage
+
+    res.json({
+        boardList: boardList,
+        pageCount: pageCount
+    })
+})
+module.exports = router;
