980ae2ede6ee08f831b71f6b925a57f90bf8f7cd
BrotherZin <ahj13k@naver.com>
2022-05-25 11:17:34 +0900

    0525/ 컴포넌트 하는법 배움

---
+++ board/client/src/components/MjcHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <v-layout
+      ><div class="logo" @click="moveMain">
+        이랏샤이마세 ~ 고레와 안형진 게시판 데스 !
+      </div>
+      <v-spacer></v-spacer>
+      <div>안형진</div></v-layout
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    moveMain() {
+      this.$router.push("/board");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  padding: 10px 20px;
+  box-shadow: 0 6px #eee;
+}
+.header .logo {
+  cursor: pointer;
+}
+</style>
--- board/server/model.js
+++ board/server/model.js
@@ -41,7 +41,6 @@ Board.belongsTo(User, {
 })
 
 
-
 sequelize.sync({
     alter: true
 })
